a323a12b4135b74a9eecbc807bd8f7a615a2e8f2
Tam Thai <hoangtam3062002@gmail.com>
2023-02-23 10:01:12 +0700

    #Add contraints to Book, title and author pair is unique

--- src/main/java/com/example/persistence/model/Book.java
+++ src/main/java/com/example/persistence/model/Book.java
@@ -12,6 +12,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -23,7 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-// @Table(name = "book")
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "title", "author" }) })
 public class Book implements Serializable {
 
     @Id
