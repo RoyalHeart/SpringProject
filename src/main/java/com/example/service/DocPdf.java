8d5c42faf20f8375ba366646a5b4ce29fa0b188c
Tam Thai <hoangtam3062002@gmail.com>
2023-02-28 09:25:04 +0700

    #Update export directly to PDF

--- src/main/java/com/example/service/DocPdf.java
+++ src/main/java/com/example/service/DocPdf.java
@@ -1,7 +1,6 @@
 package com.example.service;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,13 +8,13 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-
 import com.example.persistence.model.Book;
 import com.example.persistence.model.UserDetail;
 
-import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
 import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import fr.opensagres.xdocreport.converter.ConverterTypeTo;
+import fr.opensagres.xdocreport.converter.ConverterTypeVia;
+import fr.opensagres.xdocreport.converter.Options;
 import fr.opensagres.xdocreport.core.XDocReportException;
 import fr.opensagres.xdocreport.document.IXDocReport;
 import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
@@ -43,23 +42,25 @@ public class DocPdf {
         fieldsMetadata.saveXML(new FileOutputStream(xmlFieldsFile), true);
     }
 
-    public static void exportPdf(String inputPath, String exportPath) throws IOException, XDocReportException {
-
-        // 1) Load DOCX into XWPFDocument
-        InputStream in = new FileInputStream(new File(inputPath));
-        // IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,
-        // TemplateEngineKind.Velocity);
-        XWPFDocument document = new XWPFDocument(in);
-
-        // 2) Prepare Pdf options
-        PdfOptions options = PdfOptions.create();
-
-        // 3) Convert XWPFDocument to Pdf
+    public static void exportPdf(List<Book> books, UserDetail userDetail, String exportPath)
+            throws IOException, XDocReportException {
+        InputStream in = DocPdf.class
+                .getResourceAsStream("/books.docx");
+        IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,
+                TemplateEngineKind.Velocity);
+        FieldsMetadata fieldsMetadata = report.createFieldsMetadata();
+        fieldsMetadata.load("user", UserDetail.class);
+        fieldsMetadata.load("books", Book.class, true);
+        IContext context = report.createContext();
+        context.put("books", books);
+        context.put("user", userDetail);
         OutputStream out = new FileOutputStream(new File(exportPath));
-        PdfConverter.getInstance().convert(document, out, options);
+        PdfOptions pdfOptions = PdfOptions.create();
+        Options options = Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.XWPF).subOptions(pdfOptions);
+        report.convert(context, options, out);
     }
 
-    public static OutputStream exportDoc(List<Book> books, UserDetail userDetail, String path)
+    public static void exportDoc(List<Book> books, UserDetail userDetail, String path)
             throws IOException, XDocReportException {
         InputStream in = DocPdf.class
                 .getResourceAsStream("/books.docx");
@@ -72,9 +73,7 @@ public class DocPdf {
         context.put("books", books);
         context.put("user", userDetail);
         OutputStream out = new FileOutputStream(new File(path));
-        // report.convert(context, Options.getTo("PDF"), out);
         report.process(context, out);
-        return out;
     }
 
     public static void main(String[] args) {
@@ -95,7 +94,8 @@ public class DocPdf {
             userDetail.setUser_role("ADMIN");
             String path = "project_out.docx";
             DocPdf.exportDoc(books, userDetail, path);
-            DocPdf.exportPdf("C:\\Users\\Admin\\Downloads\\Books_27.02.2023_15.33.25.docx", "/hello.pdf");
+            // DocPdf.exportPdf("C:\\Users\\Admin\\Downloads\\Books_27.02.2023_15.33.25.docx",
+            // "/hello.pdf");
         } catch (Exception e) {
             e.printStackTrace();
         }
