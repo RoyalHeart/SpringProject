c681e03a8ac93b3b41c70991939aa0895a2d0294
Tam Thai <hoangtam3062002@gmail.com>
2023-03-01 14:21:01 +0700

    #Add try scheduling

--- src/main/java/com/example/service/scheduler/Job.java
+++ src/main/java/com/example/service/scheduler/Job.java
@@ -2,18 +2,17 @@ package com.example.service.scheduler;
 
 import java.util.logging.Logger;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Job implements Runnable {
 	public enum JobPriority {
 		HIGH,
 		MEDIUM,
 		LOW
 	}
-	private Logger logger = Logger.getLogger(Job.class.getName());
+
+	private static Logger logger = Logger.getLogger(Job.class.getName());
 	public String name;
 	public JobPriority jobPriority;
 
@@ -22,7 +21,7 @@ public class Job implements Runnable {
 		logger.info(">>> " + jobPriority + name);
 	};
 
-	public Job(String name, JobPriority jobPriority){
+	public Job(String name, JobPriority jobPriority) {
 		this.name = name;
 		this.jobPriority = jobPriority;
 	}
--- src/main/java/com/example/service/scheduler/PriorityJobScheduler.java
+++ src/main/java/com/example/service/scheduler/PriorityJobScheduler.java
@@ -6,13 +6,13 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.logging.Logger;
 
-import org.junit.Test;
+import org.springframework.stereotype.Service;
 
 import com.example.service.scheduler.Job.JobPriority;
 
 public class PriorityJobScheduler {
 
-    private Logger logger = Logger.getLogger(PriorityJobScheduler.class.getName());
+    private static Logger logger = Logger.getLogger(PriorityJobScheduler.class.getName());
     private ExecutorService priorityJobPoolExecutor;
     private ExecutorService priorityJobScheduler = Executors.newSingleThreadExecutor();
     private PriorityBlockingQueue<Job> priorityQueue;
@@ -24,14 +24,7 @@ public class PriorityJobScheduler {
                 Comparator.comparing(Job::getJobPriority));
         priorityJobScheduler.execute(() -> {
             while (true) {
-                try {
-                    priorityJobPoolExecutor.execute(priorityQueue.take());
-                    logger.info(getClass().getEnclosingMethod().getName() + ">>>" + priorityQueue.size());
-                } catch (InterruptedException e) {
-                    logger.severe("> Error:" + e.getMessage());
-                    // exception needs special handling
-                    break;
-                }
+                priorityJobPoolExecutor.execute(priorityQueue.poll());
             }
         });
     }
@@ -40,14 +33,15 @@ public class PriorityJobScheduler {
         priorityQueue.add(job);
     }
 
-    @Test
-    public void whenMultiplePriorityJobsQueued_thenHighestPriorityJobIsPicked() {
+    public static void test() throws InterruptedException {
         Job job1 = new Job("Job1", JobPriority.LOW);
         Job job2 = new Job("Job2", JobPriority.MEDIUM);
         Job job3 = new Job("Job3", JobPriority.HIGH);
         Job job4 = new Job("Job4", JobPriority.MEDIUM);
         Job job5 = new Job("Job5", JobPriority.LOW);
         Job job6 = new Job("Job6", JobPriority.HIGH);
+        Job job7 = new Job("Job7", JobPriority.MEDIUM);
+        Job job8 = new Job("Job8", JobPriority.HIGH);
 
         PriorityJobScheduler pjs = new PriorityJobScheduler(
                 1, 10);
@@ -58,5 +52,7 @@ public class PriorityJobScheduler {
         pjs.scheduleJob(job4);
         pjs.scheduleJob(job5);
         pjs.scheduleJob(job6);
+        pjs.scheduleJob(job7);
+        pjs.scheduleJob(job8);
     }
 }
