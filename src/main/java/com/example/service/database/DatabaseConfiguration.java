0157c354af4c2c0d0a578c772654a43b153b3f86
Tam Thai <hoangtam3062002@gmail.com>
2023-03-01 10:55:41 +0700

    #Update folder structure, add test case

--- src/main/java/com/example/service/database/DatabaseConfiguration.java
+++ src/main/java/com/example/service/database/DatabaseConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.database;
+package com.example.service.database;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
