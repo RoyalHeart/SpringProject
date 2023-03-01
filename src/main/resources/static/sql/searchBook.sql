dbd78593d36c808af804b6a2db06313ca9f78127
Tam Thai <hoangtam3062002@gmail.com>
2023-03-01 09:05:58 +0700

    #Update search can have from, to range query

--- src/main/resources/static/sql/searchBook.sql
+++ src/main/resources/static/sql/searchBook.sql
@@ -2,13 +2,19 @@ SELECT * FROM book
 /*BEGIN*/
   WHERE
   /*IF author != null */
-        author = /*author*/'Tom'
+  author = /*author*/'Tom'
   /*END*/
   /*IF title != null */
   OR title = /*title*/'Mindset'
   /*END*/
   /*IF published != null */
-  OR published = /*published*/2002
+   OR published =/*published*/2002
+  /*END*/
+  /*IF from != 0 */
+   OR published >=/*from*/2002
+  /*END*/
+  /*IF to != 0 */
+   AND published <=/*to*/2002
   /*END*/
 /*END*/
 ORDER BY ID ASC
