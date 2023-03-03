5e5668f290a836e8ba8249b027c62bdb8baa397b
Tam Thai <hoangtam3062002@gmail.com>
2023-03-03 15:59:31 +0700

    #Delete mock test for doc/pdf

--- src/main/java/com/example/service/import_export/DocPdf.java
+++ src/main/java/com/example/service/import_export/DocPdf.java
@@ -100,30 +100,4 @@ public class DocPdf {
         report.process(context, out);
     }
 
-    public static void main(String[] args) {
-        try {
-            List<Book> books = new ArrayList<Book>();
-            Book book = new Book();
-            book.setAuthor("Tome");
-            book.setTitle("Hello world");
-            book.setPublished((short) 2002);
-            books.add(book);
-            for (int i = 0; i < 400; i++) {
-                book = new Book();
-                book.setAuthor("Lily");
-                book.setTitle("World");
-                book.setPublished((short) 2002);
-                books.add(book);
-            }
-            UserDetail userDetail = new UserDetail();
-            userDetail.setUsername("admin");
-            userDetail.setUser_role("ADMIN");
-            String pathDoc = "project_out.docx";
-            String pathPdf = "project_out.pdf";
-            // DocPdf.exportDoc(books, userDetail, pathDoc);
-            DocPdf.exportPdf(books, userDetail, pathPdf);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
