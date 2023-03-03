c281b18c3ac82785892bc515c8c25bf24b6426df
Tam Thai <hoangtam3062002@gmail.com>
2023-03-03 08:52:01 +0700

    #Add connection to SQL server

--- src/main/java/com/example/service/database/ConnectionProviderImpl.java
+++ src/main/java/com/example/service/database/ConnectionProviderImpl.java
@@ -11,18 +11,26 @@ public class ConnectionProviderImpl implements ConnectionProvider {
     private Logger logger = Logger.getLogger(ConnectionProviderImpl.class.getName());
     private DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration();
 
-    private String url = "jdbc:postgresql://localhost:5432/postgres";
+    private String postgreUrl = "jdbc:postgresql://localhost:5432/postgres";
 
-    private String username = "postgres";
+    private String postgreUsername = "postgres";
 
-    private String password = "admin";
+    private String postgrePassword = "admin";
+
+    private String sqlServerUrl = "jdbc:sqlserver://localhost:1433;DatabaseName=BookDatabase";
+
+    private String sqlServerUsername = "sqlserver";
+
+    private String sqlServerPassword = "admin";
 
     @Override
     public Connection getConnection() {
         Connection conn = null;
         try {
-            logger.info(">>> getConnection(): connected to PostgreSQL");
-            conn = DriverManager.getConnection(url, username, password);
+            // logger.info(">>> getConnection(): connected to PostgreSQL");
+            // conn = DriverManager.getConnection(postgreUrl, postgreUsername, postgrePassword);
+            logger.info(">>> getConnection(): connected to SqlServer");
+            conn = DriverManager.getConnection(sqlServerUrl, sqlServerUsername, sqlServerPassword);
         } catch (SQLException e) {
             logger.severe(">>> getConnection() error:" + e.getMessage());
         }
