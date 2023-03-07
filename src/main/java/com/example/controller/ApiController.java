0131357856e7690d2da30a0785a34f85e3d33e10
Tam Thai <hoangtam3062002@gmail.com>
2023-03-07 13:55:30 +0700

    #Update request mapping to get and post

--- src/main/java/com/example/controller/ApiController.java
+++ src/main/java/com/example/controller/ApiController.java
@@ -5,12 +5,7 @@ import java.util.List;
 
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.example.persistence.model.Book;
 import com.example.service.BookService;
 
@@ -20,7 +15,7 @@ public class ApiController {
     @Autowired
     BookService bookService;
 
-    @RequestMapping(value = "/api", method = RequestMethod.GET)
+    @GetMapping("/api")
     public String hello() {
         return "Hello, this is the Rest API service of the books management system\n" +
                 "Here are the rules to connect and get access\n" +
@@ -29,12 +24,12 @@ public class ApiController {
                 "Post at /api/books\n";
     }
 
-    @RequestMapping(value = "/api/books", method = RequestMethod.GET)
+    @GetMapping("/api/books")
     public List<Book> getAllBooks() {
         return bookService.findAll();
     }
 
-    @RequestMapping(value = "/api/books", method = RequestMethod.POST)
+    @PostMapping("/api/books")
     public Book postBook(@RequestBody Book book) {
         logger.info(">>> postBook():" + book);
         if (book.getImported() == null) {
@@ -44,12 +39,12 @@ public class ApiController {
         return book;
     }
 
-    @RequestMapping(value = "/api/books/{id}", method = RequestMethod.GET)
+    @GetMapping("/api/books/{id}")
     public Book getBook(@PathVariable(required = true) long id) {
         return bookService.findOne(id).orElse(null);
     }
 
-    @RequestMapping(value = "/api/books/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/api/books/{id}")
     public String deleteBook(@PathVariable(required = true) long id) {
         bookService.delete(id);
         return "Delete book " + id + " successfully";
