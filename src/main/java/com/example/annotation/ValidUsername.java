48d2a93da33a632429a6dd9e83bc093ea19bd094
Tam Thai <hoangtam3062002@gmail.com>
2023-03-07 14:04:35 +0700

    #Add user defined annotation

---
+++ src/main/java/com/example/annotation/ValidUsername.java
@@ -0,0 +1,20 @@
+package com.example.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.example.annotation.impl.ValidUsernameImpl;
+
+@Target( {ElementType.FIELD})
+@Retention( RetentionPolicy.RUNTIME)
+@Constraint( validatedBy = ValidUsernameImpl.class)
+public @interface ValidUsername {
+    String message() default "";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
